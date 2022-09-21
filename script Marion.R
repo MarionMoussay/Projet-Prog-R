4c5dbe0b0d0d9366d529a9bc01d703a941c8efbb
MarieJoigneau <marie.joigneau@gmail.com>
2022-09-21 17:11:44 +0200

    j'ai rajouté mes lignes de code : par contre je dois les changer en data.table + regrouper plus de couleurs

--- script Marion.R
+++ script Marion.R
@@ -8,8 +8,27 @@ require(corrplot)
 
 stars <- read.table("data/stars.csv", sep=",", header=TRUE)
 str(stars)
+
+# Nettoyage de Marie: ----------------------------
 stars$Star.type <- as.factor(stars$Star.type)
 stars$Star.color <- as.factor(stars$Star.color)
+stars$Spectral.Class <- as.factor(stars$Spectral.Class)
+
+levels(stars$Star.type)
+levels(stars$Star.color)
+levels(stars$Spectral.Class)
+
+stars$Star.color <- as.character(stars$Star.color)
+
+stars$Star.color[which(stars$Star.color=="Blue ")] <- "Blue"
+stars$Star.color[c(which(stars$Star.color=="Blue white"),(which(stars$Star.color=="Blue white ")),(which(stars$Star.color=="Blue White")),(which(stars$Star.color=="Blue-White")))] <- "Blue-white"
+stars$Star.color[which(stars$Star.color=="white")] <- "White"
+stars$Star.color[which(stars$Star.color=="yellowish")] <- "Yellowish"
+
+stars$Star.color <- as.factor(stars$Star.color)
+levels(stars$Star.color)
+
+# -------------------------------------------------
 
 # On traite utilise pas couleur qui est elle même expliquée par la température:
 levels(stars$Star.color) <- 1:19
