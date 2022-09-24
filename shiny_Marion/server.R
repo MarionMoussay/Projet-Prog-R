4ad9c4ff91dd7445edfea433b919c555e1b93b5d
MarionMoussay <marion.moussay@hotmail.fr>
2022-09-24 12:21:17 +0200

    cah

--- shiny_Marion/server.R
+++ shiny_Marion/server.R
@@ -19,33 +19,21 @@ shinyServer(function(input, output) {
         awesomeRadio(
             inputId = "choix_ultrametric",
             label = "Choisissez la distance ultramétrique souhaitée :", 
-            choices = list("Distance de Ward"="ward.D", "Saut minimal" ="simple", "Saut maximal" ="complete")
+            choices = list("Distance de Ward"="ward.D", "Saut minimal" ="single", "Saut maximal" ="complete")
         )
     })
+    
     matrice <- reactive({matrice <- stars %>% select(-c(5,6,7))})
     
     cah <- reactive({
-        d <-  dist(matrice())
-        if (input$choix_ultrametric == "ward.D") {
-            cah <- hclust(d, method = "ward.D")
-        } else if (input$choix_ultrametric == "simple"){
-            cah <- hclust(d, method = "simple")
-        } else {
-            cah <- hclust(d, method = "complete")
-        }
+        d <- dist(matrice())
+        cah <- hclust(d, method=input$choix_ultrametric)
         cah
     })
     output$fct_perte_coude <- renderPlotly({
         data <- data.frame("inertie" = rev(cah()$height)[1:20], "clusters" = 1:20)
-
-        diff <- c()
-        for (i in 2:10){
-            diff <- cbind(diff, data$inertie[i-1]-data$inertie[i])
-        }
-        nb <-  which.min(diff)-1
-        graph <- ggplot(data, aes(y= inertie, x=clusters)) + geom_line(col="#75b8d1")+geom_point(col="#75b8d1") +labs(title="Fonction de perte")+ylab("Inertie intra-groupe")+ xlab("Nombre de clusters") + theme_minimal() + geom_vline(xintercept = nb, linetype="dotted", color = "#75b8d1", size=0.5)
+        graph <- ggplot(data, aes(y= inertie, x=clusters)) + geom_line(col="#75b8d1")+geom_point(col="#75b8d1") +labs(title="Fonction de perte")+ylab("Inertie intra-groupe")+ xlab("Nombre de clusters") + theme_minimal()
         ggplotly(graph)
-
     })
     
     output$fct_perte_gap <- renderPlotly({
@@ -53,7 +41,29 @@ shinyServer(function(input, output) {
     })
     
     output$fct_perte_silhouette <- renderPlotly({
-        fviz_nbclust(matrice(), kmeans, "silhouette", k.max = 10) + labs(subtitle = "Silhouette method")
+        fviz_nbclust(matrice(), kmeans, method="silhouette", k.max = 10) + labs(subtitle = "Silhouette method")
     })
     
+    output$plot_dendogramme <- renderPlotly({
+        ggplotly(fviz_dend(cah(), k = input$nb_clusters, show_labels = FALSE, rect = TRUE)+theme(legend.position = "none"))
+    })
+
+    output$hr_diag_clusters <- renderPlotly({
+        km <- kmeans(matrice(), centers = input$nb_clusters)
+        df_clust <- matrice() %>% bind_cols(cluster = as.factor(km$cluster))
+
+
+        graph <- ggplot(data = df_clust) +
+            geom_point(aes(x = Temperature.K, y = Absolute_Magnitude.Mv, color =cluster)) +
+            scale_y_reverse() +
+            scale_x_reverse() +
+            xlab("Temperature (K)") +
+            ylab("Magnitude absolue (Mv)") +
+            labs(title="Clusters CAH")+
+            scale_fill_identity()+
+            theme_bw() +
+            theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+            theme(plot.title = element_text(hjust = 0.5,size=9))
+        ggplotly(graph)
+    })
 })
